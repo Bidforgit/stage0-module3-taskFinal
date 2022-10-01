a555f7ea626a609917e1e55220c9d6e51b6a8d6a
Bidforgit <anuar.karimow@gmail.com>
2022-10-01 11:35:40 +0600

    +

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        String msg = number > 0 ? "true" : "false";
+        System.out.println(msg);
     }
 }
